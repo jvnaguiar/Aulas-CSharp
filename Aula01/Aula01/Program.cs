013f7cc2f5b8b25efec5fb073ecf4b32fcaaf6dc
joao.vnaguiar <joao.vnaguiar@TIT0533397W10-1>
2025-02-25 19:27:31 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("João");
+
+
+//string firstName = "João";
+//string lastName = "Nascimento";
+//string surName = "Aguiar";
+
+ //Console.WriteLine("Meu nome completo é " + firstName +" "+" " + lastName +  surName );
+
+//int idade = 29;
+
+//Console.WriteLine($"Meu nome completo é {firstName} {lastName} {surName} e a minha idade {idade}");
+
+Console.WriteLine("Escreva seu nome");
+
+string nome = Console.ReadLine();
+Console.WriteLine($"Olá  {nome}, boa noite !!");
+
+Console.ReadKey();
